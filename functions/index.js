c38e2eda37a7cef75239833ee50f47905008fc96
woisme <mywoisme@gmail.com>
2017-09-25 12:38:27 +0100

    Initialised functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const functions = require('firebase-functions');
+const firestore = require('firebase-functions/lib/providers/datastore');
 const admin = require('firebase-admin')
 admin.initializeApp(functions.config().firebase)
 const ref = admin.database().ref();
 
 // Copy a value when it is changed or created
-exports.makeCopy = functions.database.ref('/profiles/{pushId}/deck/{pushId}/card')
+exports.makeCopy = functions.database.ref('/profiles/{pushId}/deks/{pushId}/cards/{pushId}')
     .onWrite(event => {
         const copy = event.data.val();
         return event.data.ref.parent.child('/profiles/river').set(copy);
