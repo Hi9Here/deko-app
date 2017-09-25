fe793f6b10cc3f39854e47f5eecd7cf9ece652c6
woisme <mywoisme@gmail.com>
2017-09-25 12:46:55 +0100

    changes to function to help copy data

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp(functions.config().firebase)
 const ref = admin.database().ref();
 
 // Copy a value when it is changed or created
-exports.makeCopy = functions.database.ref('/profiles/{pushId}/deks/{pushId}/cards/{pushId}')
+exports.makeCopy = functions.database.ref('/profiles/{anyDocument}/deks/{anyDocument}/cards/{anyDocument}')
     .onWrite(event => {
         const copy = event.data.val();
         return event.data.ref.parent.child('/profiles/river').set(copy);
