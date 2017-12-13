29598515ca00d6a51c346cde5992aedbc77426b6
Marcus Hitchins <marcus7777@gmail.com>
2017-12-13 15:50:19 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -146,7 +146,7 @@ const languageService = functions.firestore.document('Profiles/{pid}').onWrite(e
 const imageService = functions.firestore.document('Profiles/{pid}/cards/{cardId}').onWrite(event => {
   console.log(event.data.data())
   if (!event.data.data().image) {
-    functions.firestore.document('Profiles/'+event.params.pid+'cards/'+event.params.cardId).set({image:"hi"}, {merge:true})
+    functions.firestore.document('Profiles/'+event.params.pid+'cards/'+event.params.cardId).ref.set({image:"hi"}, {merge:true})
   }
   // get title
   // get find image matching title
