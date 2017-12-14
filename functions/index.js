f7f8cb4e1b7d24d807277fda8647345bce25209b
Marcus Hitchins <marcus7777@gmail.com>
2017-12-14 14:31:38 +0000

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -201,7 +201,7 @@ app.get("/pik/deko/Welcome,%F0%9F%98%8E%20tap%20here%20and%20I'll%20give%20you%2
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   
   //Profiles/5dlcFyFDFMzhXFev4Inl/cards/Jvo3gk13xiZz9aAajYCw
-  db.collection('Profiles').docs("5dlcFyFDFMzhXFev4Inl").collection('cards').docs("Jvo3gk13xiZz9aAajYCw").get().then(doc => {
+  db.collection('Profiles').doc("5dlcFyFDFMzhXFev4Inl").collection('cards').doc("Jvo3gk13xiZz9aAajYCw").get().then(doc => {
     console.log('Welcome =>', doc.data())
     res.json(doc.data())
   }).catch(e => {
